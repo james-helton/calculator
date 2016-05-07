29351e238bf7f67fbd49d474ed92f14f474d596d
James Helton <trealtrix@gmail.com>
2016-05-07 19:36:59 -0400

    Create subtract, multiply, and divide

--- app/models/calc.rb
+++ app/models/calc.rb
@@ -1,6 +1,17 @@
 class Calc
+  def add(num1, num2)
+    num1 + num2
+  end
 
-	def add(num1, num2)
-		num1 + num2
-	end
+  def subtract(num1, num2)
+    num1 - num2
+  end
+
+  def divide(num1, num2)
+    num1 / num2
+  end
+
+  def multiply(num1, num2)
+    num1 * num2
+  end
 end
