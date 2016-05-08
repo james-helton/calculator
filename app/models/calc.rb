6f8d68a4d821467aa8770e5517e65fa1eb320590
James Helton <trealtrix@gmail.com>
2016-05-07 20:00:05 -0400

    Modify add and subtract to take unlimited arguments

--- app/models/calc.rb
+++ app/models/calc.rb
@@ -1,10 +1,14 @@
 class Calc
-  def add(num1, num2)
-    num1 + num2
+  def add(augend, *addend)
+    sum = augend
+    addend.each { |x| sum += x }
+    sum
   end
 
-  def subtract(num1, num2)
-    num1 - num2
+  def subtract(minuend, *subtrahend)
+    difference = minuend
+    subtrahend.each { |x| difference -= x }
+    difference
   end
 
   def divide(num1, num2)
